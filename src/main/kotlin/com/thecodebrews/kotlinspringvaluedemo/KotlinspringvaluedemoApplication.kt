98365d5dbb3c187181097bc1337aa9826e856874
billydh <dharmawan.billy@gmail.com>
2020-11-27 16:57:48 +1100

    skeleton from Spring Initializr

---
+++ src/main/kotlin/com/thecodebrews/kotlinspringvaluedemo/KotlinspringvaluedemoApplication.kt
@@ -0,0 +1,11 @@
+package com.thecodebrews.kotlinspringvaluedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinspringvaluedemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinspringvaluedemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/thecodebrews/kotlinspringvaluedemo/KotlinspringvaluedemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.thecodebrews.kotlinspringvaluedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinspringvaluedemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
